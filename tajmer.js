093ad9bba90ff8eb8355dfdeab90f0b6871c9841
dalibormitic <35170405+dalibormitic@users.noreply.github.com>
2018-01-30 21:25:16 +0100

    Ubacen tajmer

---
+++ tajmer.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("March 31, 2018 18:00:00").getTime();
+
+var x = setInterval(function countdown() {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("dani").innerHTML = days;
+    document.getElementById("sati").innerHTML = hours;
+    document.getElementById("minuti").innerHTML = minutes;
+    document.getElementById("sekunde").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Hakaton je počeo!";
+    }
+}, 1000);
